75c4a0f1645cc05770cdcf8e86652af066011326
annabruzaca <bebruzaca@hotmail.com>
2020-08-09 18:59:55 -0300

    +código create-post.ts

--- Front/src/app/create-post/create-post.page.ts
+++ Front/src/app/create-post/create-post.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { ToastController} from '@ionic/angular';
+import { CreatePostService } from '../services/create-post.service';
 
 @Component({
   selector: 'app-create-post',
@@ -7,7 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePostPage implements OnInit {
 
-  constructor() { }
+createPostForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder, public toastController: ToastController, public createPostService: CreatePostService) {
+      this.createPostForm = this.formBuilder.group({
+          course: [null, [Validators.required, Validators.minLength(3)]],
+          content: [null, [Validators.required, Validators.minLength(3)]],
+          //tag:[null],
+      });
+   }
+
+   async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Post criado com sucesso!',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  submitForm(form) {
+      console.log(form);
+      console.log(form.value);
+//      this.postService.createPost(body).subscribe(
+//        (res) => {console.log(res);
+//        }, (err) => {console.log(err); })
+      window.location.href="/tabs/home";
+  }
 
   ngOnInit() {
   }
