2274b7546c472640059abfdf76ad3f590823dea9
annabruzaca <bebruzaca@hotmail.com>
2020-08-09 00:46:29 -0300

    +toast *botão postar

--- Front/src/app/post/post.page.ts
+++ Front/src/app/post/post.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { ToastController} from '@ionic/angular';
 
 
 @Component({
@@ -11,18 +12,26 @@ export class PostPage implements OnInit {
 
 postForm: FormGroup;
 
-  constructor(public formBuilder: FormBuilder) {
+  constructor(public formBuilder: FormBuilder, public toastController: ToastController) {
       this.postForm = this.formBuilder.group({
-          class:[null, [Validators.required, Validators.minLength(3)]],
-          content:[null, [Validators.required, Validators.minLength(3)]],
+          course: [null, [Validators.required, Validators.minLength(3)]],
+          content: [null, [Validators.required, Validators.minLength(3)]],
           //tag:[null],
       });
    }
-   submitForm(form) {
-       console.log(form);
-       console.log(form.value);
-       window.location.href="/home";
-   }
+
+   async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Post criado com sucesso!',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  submitForm(form) {
+      console.log(form);
+      console.log(form.value);
+  }
 
   ngOnInit() {
   }
