0ee1f45011dc389f96de1968622fa5ab6b26ecbc
Arthur <arthur.albuquerque.1995@gmail.com>
2020-08-08 23:08:56 -0300

    + Landing page com elementos nao centralizados

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
   {
     path: 'cadastro',
     loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'landing',
+    loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)
   }
 
 ];
---
+++ Front/src/app/landing/landing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LandingPageRoutingModule } from './landing-routing.module';
+
+import { LandingPage } from './landing.page';
+import { LogoComponent } from './../components/logo/logo.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LandingPageRoutingModule
+  ],
+  declarations: [LandingPage, LogoComponent]
+})
+export class LandingPageModule {}
