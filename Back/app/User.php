15690149f5cc004a109ae75d58b586fba9ee8325
Lucas Pereira <olucasper@gmail.com>
2020-08-09 20:50:08 -0300

    +passport initial config

--- Back/app/User.php
+++ Back/app/User.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Http\Request;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
