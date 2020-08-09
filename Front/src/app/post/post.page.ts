cc9b2812c2b654b95b3731a8decfe99b0f4cbd5a
annabruzaca <bebruzaca@hotmail.com>
2020-08-09 19:01:07 -0300

    -código post.ts

--- Front/src/app/post/post.page.ts
+++ Front/src/app/post/post.page.ts
@@ -11,32 +11,8 @@ import { PostService } from '../services/post.service';
 })
 export class PostPage implements OnInit {
 
-postForm: FormGroup;
-
   constructor(public formBuilder: FormBuilder, public toastController: ToastController, public postService: PostService) {
-      this.postForm = this.formBuilder.group({
-          course: [null, [Validators.required, Validators.minLength(3)]],
-          content: [null, [Validators.required, Validators.minLength(3)]],
-          //tag:[null],
-      });
-   }
 
-   async presentToast() {
-    const toast = await this.toastController.create({
-      message: 'Post criado com sucesso!',
-      duration: 2000
-    });
-    toast.present();
-  }
-
-  submitForm(form) {
-      console.log(form);
-      console.log(form.value);
-//      this.postService.createPost(body).subscribe(
-//        (res) => {console.log(res);
-//        }, (err) => {console.log(err); })
-      window.location.href="/tabs/home";
-  }
 
   ngOnInit() {
   }
