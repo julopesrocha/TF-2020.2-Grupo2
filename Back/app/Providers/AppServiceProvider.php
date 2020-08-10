d600a1a6abb2169784c6abb5cde425389e6dabed
Lucas Pereira <olucasper@gmail.com>
2020-08-10 00:19:40 -0300

    +faker install

--- Back/app/Providers/AppServiceProvider.php
+++ Back/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Faker\Generator as FakerGenerator;
+use Faker\Factory as FakerFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(\Faker\Generator::class, function(){
+            return \Faker\Factory::create('pt_BR');
+        });
     }
 
     /**
