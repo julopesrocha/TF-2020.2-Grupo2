56dd96fb94f51bef9f7b956b82191e1c16f0fc5b
Lucas Pereira <olucasper@gmail.com>
2020-08-10 01:37:24 -0300

    #better seed names

--- Back/database/factories/PostFactory.php
+++ Back/database/factories/PostFactory.php
@@ -8,8 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(Post::class, function (Faker $faker) {
     return [
         'content'=>$faker->text,
-        'teacher'=>$faker->name,
-        'course'=>$faker->randomElement($array = array ('Cálculo I','Circuitos Lógicos','Ética II','Ciência da Computação','Fotografia','Teoria do Design I','Eletromagnetismo','Banco de Dados','Álgebra Linear II')),
+        'teacher'=>$faker->firstName.' '.$faker->lastName,
+        'course'=>$faker->randomElement($array = array ('Cálculo I','Circuitos Lógicos','Ética II','Anatomia','Fotografia','Teoria do Design I','Eletromagnetismo','Banco de Dados','Álgebra Linear II')),
         'tag'=>$faker->randomElement($array = array ('Feedback','Recomendações','Dúvidas','Outros'))
     ];
 });
--- Back/database/factories/UserFactory.php
+++ Back/database/factories/UserFactory.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->firstName.' '.$faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'degree'=> $faker->randomElement($array = array ('Pedagogia','Matemática','Jornalismo','Ciência da Computação','Física','História','Geografia','Música','Filosofia')),
