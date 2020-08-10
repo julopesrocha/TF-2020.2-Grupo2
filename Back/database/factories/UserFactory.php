16e42b2dee0cc8a4decd466f5f744e8cf860a433
Lucas Pereira <olucasper@gmail.com>
2020-08-10 00:26:55 -0300

    +user seeder with random valid degree

--- Back/database/factories/UserFactory.php
+++ Back/database/factories/UserFactory.php
@@ -21,8 +21,8 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'degree'=> $faker->randomElement($array = array ('Pedagogia','Matemática','Jornalismo','Ciência da Computação','Física','História','Geografia','Música','Filosofia')),
         'remember_token' => Str::random(10),
     ];
 });
