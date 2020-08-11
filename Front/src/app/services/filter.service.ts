92d939205b9952f3c931828a8c561e66ca243ef6
Lucas Pereira <olucasper@gmail.com>
2020-08-11 12:38:45 -0300

    +filter service function

--- Front/src/app/services/filter.service.ts
+++ Front/src/app/services/filter.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
 
-  constructor() { }
+  apiURL: string = 'http://localhost:8000/api/'
+
+  constructor(public http: HttpClient) { }
+
+  httpHeaders: any = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
+
+  filterPosts(form): Observable<any>{
+    return this.http.post(this.apiURL + 'filterPosts', form, this.httpHeaders );
+  }
+
 }
