ddd5c992971370a140a12c1ed0636059237a9940
Arthur <arthur.albuquerque.1995@gmail.com>
2020-08-12 15:36:09 -0300

    + pagina editarperfil

---
+++ Front/src/app/editarperfil/editarperfil.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editarperfil',
+  templateUrl: './editarperfil.page.html',
+  styleUrls: ['./editarperfil.page.scss'],
+})
+export class EditarperfilPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
