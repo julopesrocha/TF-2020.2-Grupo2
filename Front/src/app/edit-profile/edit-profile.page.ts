f8a88afdaaf77f1644d530ead0c5fe0e3e1685d9
annabruzaca <bebruzaca@hotmail.com>
2020-08-12 19:08:07 -0300

    +form no edit-profile

--- Front/src/app/edit-profile/edit-profile.page.ts
+++ Front/src/app/edit-profile/edit-profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.page.scss'],
 })
 export class EditProfilePage implements OnInit {
+    editDetailsForm: FormGroup;
 
-  constructor() { }
+    constructor(public formBuilder: FormBuilder, public authService: AuthService) {
 
+      this.editDetailsForm = this.formBuilder.group({
+        name: [null, [Validators.required, Validators.minLength(3)]],
+        email: [null, [Validators.required, Validators.email]],
+        password: [null, [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+        degree: [null]
+      });
+    }
   ngOnInit() {
   }
-
+  // sendEditDetails(form){
+  //   console.log(form);
+  //   console.log(form.value);
+  //   form.value.id = this.id;
+  //   this.editMode = false;
+  //   this.authService.updateDetails(this.user_id, form.value).subscribe(
+  //       (res)=>{
+  //           console.log(res);
+  //           this.userDetails(this.user);
+  //       }, (err) => {console.log(err);
+  //       }
+  //   )
+  // }
+  //
+  // toggleEdit(id){
+  //     this.user_id = id;
+  //     for( let comment of this.comments ){
+  //       if (comment.id == id){
+  //         this.textComment = comment.text;
+  //       }
+  //     }
+  //     this.editMode = true;
+  //   }
 }
