aa01fded96adcd0cb09b0fb6848906d5a6e3e5a9
Arthur <arthur.albuquerque.1995@gmail.com>
2020-08-12 10:49:04 -0300

    resolve merge

--- Front/src/app/cadastro/cadastro.page.ts
+++ Front/src/app/cadastro/cadastro.page.ts
@@ -26,13 +26,10 @@ export class CadastroPage implements OnInit {
   ngOnInit() {
   }
 
-<<<<<<< HEAD
   goToLanding() {
     this.route.navigate(['/landing']);
   }
   
-=======
->>>>>>> 825dad1d34586d29534ce44e4d22b04523e34297
   submitForm(form){
     console.log(form.value);
     this.authService.register(form.value).subscribe(
