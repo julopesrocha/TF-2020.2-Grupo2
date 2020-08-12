4f2470221e61e611dd0e72553f7ec94912f021eb
Arthur <arthur.albuquerque.1995@gmail.com>
2020-08-12 15:53:43 -0300

    + forms na pagina editarperfil

--- Front/src/app/editarperfil/editarperfil.page.ts
+++ Front/src/app/editarperfil/editarperfil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+/* implementar auth */
 
 @Component({
   selector: 'app-editarperfil',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarperfilPage implements OnInit {
 
-  constructor() { }
+  editProfileForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder) { 
+    this.editProfileForm = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(3)]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+      confirm_password: [null, [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+      degree: [null]
+    });
+  }
 
   ngOnInit() {
   }
+
+  /* submitForm(form) {
+    console.log(form.value);
+    this.authService.register(form.value).subscribe(
+      (res) => {
+        console.log(res);
+      }, (err) => {
+        console.log(err);
+      })
+  } */
 
 }
