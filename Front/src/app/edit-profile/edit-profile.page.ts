3c0ea1872e3f53bff2f0c57935f79111a4fd3270
annabruzaca <bebruzaca@hotmail.com>
2020-08-13 10:53:51 -0300

    *id //

--- Front/src/app/edit-profile/edit-profile.page.ts
+++ Front/src/app/edit-profile/edit-profile.page.ts
@@ -10,11 +10,9 @@ import { AuthService } from '../services/auth/auth.service';
 export class EditProfilePage implements OnInit {
     editDetailsForm: FormGroup;
     editMode = false;
-    user_id: number;
 
 
     constructor(public formBuilder: FormBuilder, public authService: AuthService) {
-      this.user_id = JSON.parse(localStorage.getItem('user')).id;
       this.editDetailsForm = this.formBuilder.group({
         name: [null, [Validators.minLength(3)]],
         email: [null, [Validators.email]],
@@ -24,17 +22,16 @@ export class EditProfilePage implements OnInit {
     }
   ngOnInit() {
   }
-  sendEditDetails(form){
-    console.log(form);
-    console.log(form.value);
-    form.value.id = this.id;
-    this.editMode = false;
-    this.authService.updateUser(this.user_id, form.value).subscribe(
-        (res)=>{
-            console.log(res);
-        }, (err) => {console.log(err);
-        }
-    )
-  }
+  // sendEditDetails(form){
+  //   console.log(form);
+  //   console.log(form.value);
+  //   this.editMode = false;
+  //   this.authService.updateUser(this.user_id, form.value).subscribe(
+  //       (res)=>{
+  //           console.log(res);
+  //       }, (err) => {console.log(err);
+  //       }
+  //   )
+  // }
 
 }
