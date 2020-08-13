1c1883bc58e6cde9770084c9817b78cfa172ee63
annabruzaca <bebruzaca@hotmail.com>
2020-08-13 11:57:51 -0300

    icone busca

--- Front/src/app/cadastro/cadastro.module.ts
+++ Front/src/app/cadastro/cadastro.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { CadastroPageRoutingModule } from './cadastro-routing.module';
