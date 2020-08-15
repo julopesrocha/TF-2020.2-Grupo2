8627dbdfeafda4bbb6ee77484d6a480fac7cf682
Lucas Pereira <olucasper@gmail.com>
2020-08-15 14:02:35 -0300

    +thread service headers

--- Front/src/app/services/thread.service.ts
+++ Front/src/app/services/thread.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThreadService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  apiURL:string = 'http://localhost:8000/api/';
+
+  httpHeaders: any = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
 }
