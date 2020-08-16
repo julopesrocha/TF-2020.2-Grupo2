e47b5e99c2abc3256b93f13882909e7adf6a77f3
Lucas Pereira <olucasper@gmail.com>
2020-08-16 15:49:43 -0300

    +admin seed

--- Back/database/seeds/DatabaseSeeder.php
+++ Back/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
+
+        DB::table('users')->insert([
+            'name' => 'Lucas Pereira',
+            'email' => 'lucas@email.com',
+            'password' => bcrypt('senha123'),
+            'degree' => 'Engenharia Elétrica',
+            'admin' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Anna Bruzaca',
+            'email' => 'anna@email.com',
+            'password' => bcrypt('senha123'),
+            'degree' => 'Design',
+            'admin' => 1,
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Arthur Albuquerque',
+            'email' => 'arthur@email.com',
+            'password' => bcrypt('senha123'),
+            'degree' => 'Ciência da Computação',
+            'admin' => 1,
+        ]);
     }
+
 }
