4e235d4e7d35f32d9650146c235602e7ea94845c
Arthur <arthur.albuquerque.1995@gmail.com>
2020-08-15 22:47:23 -0300

    + cria forms e começa integracao de editar posts

--- Front/src/app/edit-post/edit-post.page.ts
+++ Front/src/app/edit-post/edit-post.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+/* importar EditPostService */
 
 @Component({
   selector: 'app-edit-post',
@@ -7,7 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPostPage implements OnInit {
 
-  constructor() { }
+  editPostForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder, public toastController: ToastController, private route: Router) {
+
+    this.editPostForm = this.formBuilder.group({
+      course: [null, [Validators.required, Validators.minLength(3)]],
+      teacher: [null, [Validators.required, Validators.minLength(3)]],
+      content: [null, [Validators.required, Validators.minLength(3)]],
+      tag: [null],
+    });
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Post editado com sucesso!',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  submitForm(form) {
+    /* this.editPostService.createPost(form.value).subscribe(
+      (res) => {
+        console.log(res);
+        this.route.navigate(['../thread:id']);
+      }, (err) => { console.log(err); }) */
+  }
 
   ngOnInit() {
   }
