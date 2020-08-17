1878b1ead09c86343d82662fde1cfba8c87015bd
Arthur <arthur.albuquerque.1995@gmail.com>
2020-08-17 11:37:28 -0300

    + rota de volta pra home caso post seja deletado

--- Front/src/app/thread/thread.page.ts
+++ Front/src/app/thread/thread.page.ts
@@ -54,6 +54,7 @@ export class ThreadPage implements OnInit {
     this.threadService.deletePost(id).subscribe(
       (res) => {
         console.log(res);
+        this.route.navigate(['/edit-post']);
         //colocar um aviso ou toast de o post foi deletado
       }, (err) => {
         console.log(err.error);
