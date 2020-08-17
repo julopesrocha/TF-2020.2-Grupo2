0716e38254da5c46f16cb1ea213076e303cfd653
annabruzaca <bebruzaca@hotmail.com>
2020-08-17 17:55:59 -0300

    #alert seguidos page

--- Front/src/app/seguidos/seguidos.page.ts
+++ Front/src/app/seguidos/seguidos.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {AuthService} from '../services/auth/auth.service';
+import { ToastController} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-seguidos',
@@ -11,17 +13,26 @@ export class SeguidosPage implements OnInit {
 
   users = [];
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, public toastController: ToastController) { }
 
   ngOnInit() {
     this.listFollowing();
   }
 
+ //  async presentToast() {
+ //   const toast = await this.toastController.create({
+ //     message: 'Você deixou de seguir o usuário!',
+ //     duration: 2000,
+ //     color: "secondary"
+ //   });
+ //   toast.present();
+ // }
+
    unfollowUser(id){
     this.authService.unfollowUser(id).subscribe(
       (res)=>{
         console.log(res);
-        alert(res[0]);
+        alert('Você deixou de seguir o usuário!');
       },(err) =>{
         console.log(err);
         alert(err.error[0]);
